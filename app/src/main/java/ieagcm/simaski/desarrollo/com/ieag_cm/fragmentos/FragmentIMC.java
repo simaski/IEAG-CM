6699cb17ee2bc72cbf0cc4b042eaee314c6d9fd4
klomp60 <klomp60@gmail.com>
2016-02-15 16:37:03 -0430

    app chef millenium cambios: agregado string calculo imc

--- app/src/main/java/ieagcm/simaski/desarrollo/com/ieag_cm/fragmentos/FragmentIMC.java
+++ app/src/main/java/ieagcm/simaski/desarrollo/com/ieag_cm/fragmentos/FragmentIMC.java
@@ -1,8 +1,10 @@
 package ieagcm.simaski.desarrollo.com.ieag_cm.fragmentos;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +24,14 @@ public class FragmentIMC extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nutricion_imc, container, false);
+
+        Resources res = getResources();
+        String texto = res.getString(R.string.tabla);
+
+        CharSequence textoInterpretado = Html.fromHtml(texto);
+
+        TextView textView = (TextView) view.findViewById(R.id.tv_intro);
+        textView.setText(textoInterpretado);
 
 
         return view;
